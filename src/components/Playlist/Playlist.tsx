f8f9153ef7ac3dea0334d7073dd316f73beede23
Jesse Nauman <jnauman619@gmail.com>
2021-09-06 13:41:13 -0500

    Add song to playlist

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -1,17 +1,42 @@
 import { selectPlaylistName, selectPlaylistId } from "../CreatePlaylist/createPlaylistSlice";
+import { selectSongId } from "../GeniusLogic/SongIdSlice";
+import { selectAccessToken } from '../../features/authorization/authorizationSlice';
 import { useEffect } from "react";
 import { useSelector } from 'react-redux';
 
 export const Playlist = () => {
-    
+    let accessToken = useSelector(selectAccessToken);
     let playlistId = useSelector(selectPlaylistId);
     let playlistName = useSelector(selectPlaylistName);
+    let songId = useSelector(selectSongId);
 
     useEffect(() => {
         const playlistDisplayName = document.getElementById('playlist-name')!;
         playlistDisplayName.innerHTML = playlistName;
     }, [playlistId])
 
+    useEffect(() => {
+        const addToPlaylist = async () => {
+            let playlistNameInput = (document.getElementById("playlist-name-input") as HTMLInputElement)
+            console.log(playlistNameInput);
+            let playlistName = playlistNameInput.value;
+            console.log(playlistName);
+    
+            console.log(accessToken);
+            const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=spotify:track:${songId}`,{
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + accessToken
+                },
+            });
+            const data = await result.json();
+            console.log(data)
+            return data
+        };
+        addToPlaylist();
+    }, [songId])
 
     return (
         <div className="w-6/12 mx-auto">
