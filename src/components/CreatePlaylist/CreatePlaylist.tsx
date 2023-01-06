d9d7942c3f97a2e7a01926b7af414da3140c1303
Jesse Nauman <jnauman619@gmail.com>
2023-01-06 18:22:08 -0500

    chore: fix dependency warnings

--- src/components/CreatePlaylist/CreatePlaylist.tsx
+++ src/components/CreatePlaylist/CreatePlaylist.tsx
@@ -1,101 +1,135 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectAccessToken, selectIsLoggedIn } from '../Authorization/authorizationSlice';
-import { selectUserId } from '../User/UserSlice';
-import { setPlaylistName, setPlaylistId, selectPlaylistName } from './createPlaylistSlice';
-import { Playlist } from '../Playlist';
-import { useEffect } from 'react';
+import React, { useCallback, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAccessToken,
+  selectIsLoggedIn,
+} from "../Authorization/authorizationSlice";
+import { selectUserId } from "../User/UserSlice";
+import {
+  setPlaylistName,
+  setPlaylistId,
+  selectPlaylistName,
+} from "./createPlaylistSlice";
+import { Playlist } from "../Playlist";
+import { useEffect } from "react";
 
 interface PlaylistsObject {
-    [key: string]: string
+  [key: string]: string;
 }
 
-const userPlaylists: PlaylistsObject = {'': '-- Select Playlist --'};
+const userPlaylists: PlaylistsObject = { "": "-- Select Playlist --" };
 
 export const CreatePlaylist = () => {
-    const isLoggedIn = useSelector(selectIsLoggedIn);
-    const accessToken = useSelector(selectAccessToken);
-    const userId = useSelector(selectUserId);
-    const playlistName = useSelector(selectPlaylistName);
-    const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const accessToken = useSelector(selectAccessToken);
+  const userId = useSelector(selectUserId);
+  const playlistName = useSelector(selectPlaylistName);
+  const dispatch = useDispatch();
 
-    let headers = new Headers([
-        ['Content-Type', 'application/json'],
-        ['Accept', 'application/json'],
-        ['Authorization', `Bearer ${accessToken}`]
-    ]);
+  const headers = useMemo(
+    () =>
+      new Headers([
+        ["Content-Type", "application/json"],
+        ["Accept", "application/json"],
+        ["Authorization", `Bearer ${accessToken}`],
+      ]),
+    [accessToken]
+  );
 
-    const createPlaylist = async () => {
-        const result = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,{
-            method: 'POST',
-            body: `{"name":"${playlistName}","public":true}`,
-            headers: headers,
-        });
-        const data = await result.json();
-        dispatch(setPlaylistId(data.id));
-        (document.getElementById("playlist-name-input") as HTMLInputElement).value = '';
-        document.getElementById("success")!.className = "text-white flex bg-green-400 border-2 px-3 ml-3 self-center rounded";
-        return data
-    };
-
-    const getPlaylists = async () => {
-        const result = await fetch(`https://api.spotify.com/v1/me/playlists`,{
-        method: 'GET',
+  const createPlaylist = async () => {
+    const result = await fetch(
+      `https://api.spotify.com/v1/users/${userId}/playlists`,
+      {
+        method: "POST",
+        body: `{"name":"${playlistName}","public":true}`,
         headers: headers,
-        });
-        const data = await result.json();
-        let arrOfPlaylists = data.items;
-        console.log(arrOfPlaylists.length);
-        for (let i=0; i < arrOfPlaylists.length; i++) {
-            if (arrOfPlaylists[i].owner.id === userId) {
-                // need name to display in dropdown
-                let aPlaylistName: string = arrOfPlaylists[i].name;
-                // need id to get tracks in playlist
-                let aPlaylistId: string = arrOfPlaylists[i].id;
-                userPlaylists[aPlaylistId] = aPlaylistName;
-            }  
-        }
-        console.log(userPlaylists);
-        let select = document.createElement('select');
-        select.className = 'border-2 border-blue-600 rounded px-1'
-        for (let [k,v] of Object.entries(userPlaylists)) {
-            let option = document.createElement('option');
-            option.value = k;
-            option.innerText = v;
-            select.appendChild(option);
-        }
-        document.getElementById('create-container')?.appendChild(select);
-        select.addEventListener('change', (e) => {
-            dispatch(setPlaylistId((e.target as HTMLSelectElement)!.value!));
-            dispatch(setPlaylistName(userPlaylists[(e.target as HTMLSelectElement)!.value!]));
-        });
-        
+      }
+    );
+    const data = await result.json();
+    dispatch(setPlaylistId(data.id));
+    (document.getElementById("playlist-name-input") as HTMLInputElement).value =
+      "";
+    document.getElementById("success")!.className =
+      "text-white flex bg-green-400 border-2 px-3 ml-3 self-center rounded";
+    return data;
+  };
+
+  const getPlaylists = useCallback(async () => {
+    const result = await fetch(`https://api.spotify.com/v1/me/playlists`, {
+      method: "GET",
+      headers: headers,
+    });
+    const data = await result.json();
+    let arrOfPlaylists = data.items;
+    console.log(arrOfPlaylists.length);
+    for (let i = 0; i < arrOfPlaylists.length; i++) {
+      if (arrOfPlaylists[i].owner.id === userId) {
+        // need name to display in dropdown
+        let aPlaylistName: string = arrOfPlaylists[i].name;
+        // need id to get tracks in playlist
+        let aPlaylistId: string = arrOfPlaylists[i].id;
+        userPlaylists[aPlaylistId] = aPlaylistName;
+      }
     }
+    console.log(userPlaylists);
+    let select = document.createElement("select");
+    select.className = "border-2 border-blue-600 rounded px-1";
+    for (let [k, v] of Object.entries(userPlaylists)) {
+      let option = document.createElement("option");
+      option.value = k;
+      option.innerText = v;
+      select.appendChild(option);
+    }
+    document.getElementById("create-container")?.appendChild(select);
+    select.addEventListener("change", (e) => {
+      dispatch(setPlaylistId((e.target as HTMLSelectElement)!.value!));
+      dispatch(
+        setPlaylistName(userPlaylists[(e.target as HTMLSelectElement)!.value!])
+      );
+    });
+  }, [dispatch, headers, userId]);
 
-    useEffect(() => {
-        if (userId) {
-            getPlaylists()
-        }
-    }, [userId])
+  useEffect(() => {
+    if (userId) {
+      getPlaylists();
+    }
+  }, [userId, getPlaylists]);
 
-    return (
-        <div className="w-6/12 mx-auto rounded">
-            {isLoggedIn && <div className="flex justify-center my-10 mx-auto">
-                <div id="create-container">
-                    <p className="text-center mb-3">Create Spotify Playlist</p>
-                    <div className="flex">
-                        <input onChange={e => dispatch(setPlaylistName(e.target.value))} id="playlist-name-input" type="text" placeholder="Playlist Name" className="text-box border-2 border-blue-600 rounded px-1" />
-                        <button onClick={createPlaylist} className="bg-blue-600 hover:bg-blue-700 rounded px-3 py-1 ml-3 text-white">Create</button>
-                        <div id="success" className="hidden">
-                            <p>&#10003;</p>
-                            <p className="text-xs ml-3">Playlist<br />Created</p>
-                        </div>
-                    </div>
-                    <p className="text-center my-9">- or -</p>
-                    <p className="text-center mb-3">Choose Existing Playlist</p>
-                </div>
-            </div>}
-            <Playlist />
+  return (
+    <div className="w-6/12 mx-auto rounded">
+      {isLoggedIn && (
+        <div className="flex justify-center my-10 mx-auto">
+          <div id="create-container">
+            <p className="text-center mb-3">Create Spotify Playlist</p>
+            <div className="flex">
+              <input
+                onChange={(e) => dispatch(setPlaylistName(e.target.value))}
+                id="playlist-name-input"
+                type="text"
+                placeholder="Playlist Name"
+                className="text-box border-2 border-blue-600 rounded px-1"
+              />
+              <button
+                onClick={createPlaylist}
+                className="bg-blue-600 hover:bg-blue-700 rounded px-3 py-1 ml-3 text-white"
+              >
+                Create
+              </button>
+              <div id="success" className="hidden">
+                <p>&#10003;</p>
+                <p className="text-xs ml-3">
+                  Playlist
+                  <br />
+                  Created
+                </p>
+              </div>
+            </div>
+            <p className="text-center my-9">- or -</p>
+            <p className="text-center mb-3">Choose Existing Playlist</p>
+          </div>
         </div>
-    )
-}
+      )}
+      <Playlist />
+    </div>
+  );
+};
--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -1,58 +1,72 @@
-import { selectPlaylistName, selectPlaylistId } from "../CreatePlaylist/createPlaylistSlice";
+import {
+  selectPlaylistName,
+  selectPlaylistId,
+} from "../CreatePlaylist/createPlaylistSlice";
 import { selectAddSongId } from "../GeniusLogic/AddSongIdSlice";
-import { selectAccessToken } from '../Authorization/authorizationSlice';
-import { useEffect } from "react";
-import { useSelector } from 'react-redux';
+import { selectAccessToken } from "../Authorization/authorizationSlice";
+import { useCallback, useEffect, useMemo } from "react";
+import { useSelector } from "react-redux";
 
 export const Playlist = () => {
-    let accessToken = useSelector(selectAccessToken);
-    let playlistId = useSelector(selectPlaylistId);
-    let songId = useSelector(selectAddSongId);
+  let accessToken = useSelector(selectAccessToken);
+  let playlistId = useSelector(selectPlaylistId);
+  let songId = useSelector(selectAddSongId);
 
-    const ul = document.getElementById('playlist-ul')!;
+  const ul = document.getElementById("playlist-ul")!;
 
-    let headers = new Headers([
-        ['Content-Type', 'application/json'],
-        ['Accept', 'application/json'],
-        ['Authorization', `Bearer ${accessToken}`]
-    ]);
+  let headers = useMemo(
+    () =>
+      new Headers([
+        ["Content-Type", "application/json"],
+        ["Accept", "application/json"],
+        ["Authorization", `Bearer ${accessToken}`],
+      ]),
+    [accessToken]
+  );
 
-    const removeFromPlaylist = async (e: MouseEvent, songId:string) => {
-        await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`,{
-            method: 'DELETE',
-            body: `{"tracks":[{"uri":"spotify:track:${songId}"}]}`,
-            headers: headers
-        });
-        let li1 = (e.target as Element).parentNode;
-        (li1 as Element).remove();
-    }
+  const removeFromPlaylist = useCallback(
+    async (e: MouseEvent, songId: string) => {
+      await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        method: "DELETE",
+        body: `{"tracks":[{"uri":"spotify:track:${songId}"}]}`,
+        headers: headers,
+      });
+      let li1 = (e.target as Element).parentNode;
+      (li1 as Element).remove();
+    },
+    [headers, playlistId]
+  );
 
-    // if playlist id changes, display playlist
-    useEffect(() => {
-        const displayPlaylist = async () => {
-            // clear out prior playlist
-            ul.innerHTML = '';
-            // get playlist's items
-            const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`,{
-                method: 'GET',
-                headers: headers
-            });
-            const data = await result.json();
-            console.log(data)
-            let arrOfSongs = data.items;
+  // if playlist id changes, display playlist
+  useEffect(() => {
+    const displayPlaylist = async () => {
+      // clear out prior playlist
+      ul.innerHTML = "";
+      // get playlist's items
+      const result = await fetch(
+        `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+        {
+          method: "GET",
+          headers: headers,
+        }
+      );
+      const data = await result.json();
+      console.log(data);
+      let arrOfSongs = data.items;
 
-            if (arrOfSongs.length !== 0) {
-                // console.log(arrOfSongs)
-                for (let i=0; i< arrOfSongs.length; i++) {
-                    let displayArtist = arrOfSongs[i].track.artists[0].name;
-                    let displayTitle = arrOfSongs[i].track.name;
-                    let displayAlbum = arrOfSongs[i].track.album.name;
-                    let albumCover = arrOfSongs[i].track.album.images[2].url;
-                    let trackId = arrOfSongs[i].track.id;
+      if (arrOfSongs.length !== 0) {
+        // console.log(arrOfSongs)
+        for (let i = 0; i < arrOfSongs.length; i++) {
+          let displayArtist = arrOfSongs[i].track.artists[0].name;
+          let displayTitle = arrOfSongs[i].track.name;
+          let displayAlbum = arrOfSongs[i].track.album.name;
+          let albumCover = arrOfSongs[i].track.album.images[2].url;
+          let trackId = arrOfSongs[i].track.id;
 
-                    let li = document.createElement('li');
-                    li.className = "song-list list-none flex whitespace-nowrap items-center my-2 px-2 py-1 bg-white shadow-md rounded";
-                    li.innerHTML = `<img class="album-cover my-auto" src="${albumCover}" />
+          let li = document.createElement("li");
+          li.className =
+            "song-list list-none flex whitespace-nowrap items-center my-2 px-2 py-1 bg-white shadow-md rounded";
+          li.innerHTML = `<img class="album-cover my-auto" src="${albumCover}" />
                                         <div class="my-auto text-sm overflow-hidden mr-5">
                                             <p class="font-semibold p-0 m-0 opacity-80">
                                                 ${displayTitle}
@@ -63,54 +77,63 @@ export const Playlist = () => {
                                             <p class="text-xs p-0 m-0 opacity-60">
                                                 ${displayAlbum}
                                             </p>
-                                        </div>`
+                                        </div>`;
 
-                    // delete btn
-                    let delBtn = document.createElement('button');
-                    delBtn.className = "bg-red-600 hover:bg-red-700 px-2 rounded text-xl text-gray-200 font-semibold ml-auto";
-                    delBtn.innerHTML = 'x';
-                    delBtn.addEventListener('click', (e) => removeFromPlaylist(e, trackId))
-                    li.appendChild(delBtn);
-                    ul.appendChild(li);
-                }
-            }
+          // delete btn
+          let delBtn = document.createElement("button");
+          delBtn.className =
+            "bg-red-600 hover:bg-red-700 px-2 rounded text-xl text-gray-200 font-semibold ml-auto";
+          delBtn.innerHTML = "x";
+          delBtn.addEventListener("click", (e) =>
+            removeFromPlaylist(e, trackId)
+          );
+          li.appendChild(delBtn);
+          ul.appendChild(li);
         }
+      }
+    };
 
-        if (playlistId) {
-            displayPlaylist();
+    if (playlistId) {
+      displayPlaylist();
+    }
+  }, [playlistId, headers, removeFromPlaylist, ul]);
+
+  // if addSongId changes
+  useEffect(() => {
+    const addToPlaylist = async () => {
+      // Add song to playlist in Spotify database
+      const result = await fetch(
+        `https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=spotify:track:${songId}`,
+        {
+          method: "POST",
+          headers: headers,
         }
-        
-    }, [playlistId])
+      );
+      const data = await result.json();
+      console.log(data);
 
-    // if addSongId changes
-    useEffect(() => {
-        const addToPlaylist = async () => {
-            // Add song to playlist in Spotify database
-            const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=spotify:track:${songId}`,{
-                method: 'POST',
-                headers: headers
-            });
-            const data = await result.json();
-            console.log(data)
+      // Retrieve info for that song
+      const request = await fetch(
+        `https://api.spotify.com/v1/tracks/${songId}?market=US`,
+        {
+          method: "GET",
+          headers: headers,
+        }
+      );
 
-            // Retrieve info for that song
-            const request = await fetch(`https://api.spotify.com/v1/tracks/${songId}?market=US`,{
-                    method: 'GET',
-                    headers: headers
-                });
-        
-            let spotifyResults = await request.json();
-            console.log(spotifyResults);
+      let spotifyResults = await request.json();
+      console.log(spotifyResults);
 
-            // display playlist
-            let displayArtist = spotifyResults.artists[0].name;
-            let displayTitle = spotifyResults.name;
-            let displayAlbum = spotifyResults.album.name;
-            let albumCover = spotifyResults.album.images[2].url;
+      // display playlist
+      let displayArtist = spotifyResults.artists[0].name;
+      let displayTitle = spotifyResults.name;
+      let displayAlbum = spotifyResults.album.name;
+      let albumCover = spotifyResults.album.images[2].url;
 
-            let li = document.createElement('li');
-            li.className = "song-list list-none flex whitespace-nowrap items-center my-2 px-2 py-1 bg-white shadow-md rounded";
-            li.innerHTML = `<img class="album-cover my-auto" src="${albumCover}" />
+      let li = document.createElement("li");
+      li.className =
+        "song-list list-none flex whitespace-nowrap items-center my-2 px-2 py-1 bg-white shadow-md rounded";
+      li.innerHTML = `<img class="album-cover my-auto" src="${albumCover}" />
                             <div class="my-auto text-sm overflow-hidden mr-5">
                                 <p class="font-semibold p-0 m-0 opacity-80">
                                     ${displayTitle}
@@ -121,33 +144,35 @@ export const Playlist = () => {
                                 <p class="text-xs p-0 m-0 opacity-60">
                                     ${displayAlbum}
                                 </p>
-                            </div>`
+                            </div>`;
 
-            let delBtn = document.createElement('button');
-            delBtn.className = "bg-red-600 hover:bg-red-700 px-2 rounded text-xl text-gray-200 font-semibold ml-auto";
-            delBtn.innerHTML = 'x';
-            delBtn.addEventListener('click', (e) => removeFromPlaylist(e, songId))
-            li.appendChild(delBtn);
-            ul.appendChild(li);
-        };
+      let delBtn = document.createElement("button");
+      delBtn.className =
+        "bg-red-600 hover:bg-red-700 px-2 rounded text-xl text-gray-200 font-semibold ml-auto";
+      delBtn.innerHTML = "x";
+      delBtn.addEventListener("click", (e) => removeFromPlaylist(e, songId));
+      li.appendChild(delBtn);
+      ul.appendChild(li);
+    };
 
-        
-        if (playlistId === '') {
-            console.log('Nothing here')
-        } else {
-            addToPlaylist();
-        }
-    }, [songId])
+    if (playlistId === "") {
+      console.log("Nothing here");
+    } else {
+      addToPlaylist();
+    }
+  }, [songId, headers, playlistId, removeFromPlaylist, ul]);
 
-    return (
-        <div className="w-6/12 mx-auto">
-            <h2 id="playlist-name" className="text-3xl text-center mt-2 mb-5 rounded">{useSelector(selectPlaylistName)}</h2>
-            <div id="list-container" className="flex justify-center rounded">
-                <img src="" alt="" />
-                <ul id="playlist-ul" className="text-left"></ul>
-            </div>
-            
-            {/* <ul id="playlist-ul" className="border-l border-r border-b border-gray-400 rounded"></ul> */}
-        </div>
-    )
-}
+  return (
+    <div className="w-6/12 mx-auto">
+      <h2 id="playlist-name" className="text-3xl text-center mt-2 mb-5 rounded">
+        {useSelector(selectPlaylistName)}
+      </h2>
+      <div id="list-container" className="flex justify-center rounded">
+        <img src="" alt="" />
+        <ul id="playlist-ul" className="text-left"></ul>
+      </div>
+
+      {/* <ul id="playlist-ul" className="border-l border-r border-b border-gray-400 rounded"></ul> */}
+    </div>
+  );
+};
